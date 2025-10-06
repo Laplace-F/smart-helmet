a961624fbf66e7848d19833fe31723b74ce24e83
bintangwayoi15-design <bintangwayoi15@gmail.com>
2025-10-06 22:01:26 +0700

    revisi

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/styles.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css','resources/css/styles.css','resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
-    ],
+    ], 
 });
