6df019c00fa8b9c3b2b0fbd48288f9b8aff7f5e3
Kristiasara18 <hmmshhtt@gmail.com>
2025-10-06 11:08:38 +0700

    revisi

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::post('/login', function (Request $request) {
+    // nanti bisa ganti dengan autentikasi sebenarnya
+    $email = $request->input('email');
+    $password = $request->input('password');
+
+    if ($email === 'admin@smarthelmet.com' && $password === '123456') {
+        return 'Login sukses!';
+    } else {
+        return back()->withErrors(['email' => 'Email atau password salah.']);
+    }
+})->name('login.submit');
+
